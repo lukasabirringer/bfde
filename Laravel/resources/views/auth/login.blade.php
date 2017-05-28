21a530919b5295d5723f9fce8cc808ee8d7c5c0a
Fabian <pecherfabian@gmail.com>
2017-05-28 17:06:32 +0200

    added new login form

--- Laravel/resources/views/auth/login.blade.php
+++ Laravel/resources/views/auth/login.blade.php
@@ -1,6 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="content">
+    <div class="row -spacing-widget-default">
+        <div class="column column--12">
+            {{> organisms-header-page }}
+        </div>
+    </div>
+    <div class="row -spacing-d -spacing-inner-a">
+        <div class="column column--2"></div>
+        <div class="column column--12 column--m-8 -align-right">
+        <form method="POST" action="{{ route('login') }}">
+            {{ csrf_field() }}
+
+            <label class="input">
+                <input type="email" name="email" class="input__field" placeholder=" " value="{{ old('email') }}" required>
+                    <span class="input__icon icon icon--mail "></span>
+                <span class="input__label">Deine E-Mail Adresse</span>
+            </label>
+
+            @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+
+            <label class="input">
+                <input type="password" class="input__field" placeholder=" " name="password" required>
+                    <span class="input__icon icon icon--password "></span> 
+                <span class="input__label">Dein Passwort</span>
+            </label>
+
+            @if ($errors->has('password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+            @endif
+
+            <button type="submit" class="button">
+                <span class="button__label ">Anmelden</span>
+            </button>
+        </form>
+            <br><br>
+            {{> molecules-input(input_label:"Dein Passwort", input_type: "password", input_icon: "password") }}
+            <br><br>
+            {{> molecules-button(button_label:"Anmelden") }}
+            <p class="-typo-copy -text-color-blue-2 -font-primary -spacing-static-c">
+                <a href="#" class="-typo-copy-link -text-color-green">Passwort vergessen?</a>
+            </p>
+            <p class="-typo-copy -text-color-blue-2 -font-primary -spacing-static-c">Noch kein Mitglied?
+                Dann <a href="#" class="-typo-copy-link -text-color-green">registriere</a> dich jetzt</p>
+        </div>
+        <div class="column column--2"></div>
+    </div>
+</div>
+
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
