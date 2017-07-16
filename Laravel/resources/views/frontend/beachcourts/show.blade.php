17865566f5fb833ae2765091da9c072d360f82fa
Lukas Birringer <lukas.a.birringer@gmail.com>
2017-07-16 21:44:25 +0200

    Added slider images to beach court detail page

--- Laravel/resources/views/frontend/beachcourts/show.blade.php
+++ Laravel/resources/views/frontend/beachcourts/show.blade.php
@@ -43,11 +43,11 @@
 					<div class="slider-image__navigation slider-image__navigation--right icon icon--arrow-right-thin"></div>
 				</div>
 				<div class="slider-image__slider">
-						<img src="https://beachfelder.de/img/dummy-image-beachcourt-detailpage.jpg" class="slider-image__image">
-					  	<img src="https://beachfelder.de/img/dummy-image-beachcourt-detailpage.jpg" class="slider-image__image">
-					  	<img src="https://beachfelder.de/img/dummy-image-beachcourt-detailpage.jpg" class="slider-image__image">
-					  	<img src="https://beachfelder.de/img/dummy-image-beachcourt-detailpage.jpg" class="slider-image__image">
-					  	<img src="https://beachfelder.de/img/dummy-image-beachcourt-detailpage.jpg" class="slider-image__image">
+						<img src="/uploads/beachcourts/{{ $beachcourt->id }}/slider/1.png" class="slider-image__image">
+						<img src="/uploads/beachcourts/{{ $beachcourt->id }}/slider/2.png" class="slider-image__image">
+						<img src="/uploads/beachcourts/{{ $beachcourt->id }}/slider/3.png" class="slider-image__image">
+						<img src="/uploads/beachcourts/{{ $beachcourt->id }}/slider/4.png" class="slider-image__image">
+						<img src="/uploads/beachcourts/{{ $beachcourt->id }}/slider/5.png" class="slider-image__image">
 				</div>
 			</div>
 		</div>
