f948348c55908435d2e59b6d97ce517b8c77ab36
Fabian <pecherfabian@gmail.com>
2017-07-25 17:54:49 +0200

    styled cookie disclaimer

--- Laravel/resources/lang/vendor/cookieConsent/de/texts.php
+++ Laravel/resources/lang/vendor/cookieConsent/de/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => 'Um das Nutzererlebnis zu steigern, verwendet diese Seite Cookies.',
+    'message' => 'Um das Nutzererlebnis zu steigern, verwendet diese Seite Cookies. Wenn Sie mehr darüber erfahren möchten, <a href="#">klicken Sie hier</a>',
     'agree' => 'Akzeptieren',
 ];
--- Laravel/resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ Laravel/resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,11 +1,9 @@
-<div class="js-cookie-consent cookie-consent">
-
-    <span class="cookie-consent__message">
-        {!! trans('cookieConsent::texts.message') !!}
-    </span>
-
-    <button class="js-cookie-consent-agree cookie-consent__agree">
-        {{ trans('cookieConsent::texts.agree') }}
-    </button>
-
+<div class="notification-sticky js-cookie-consent cookie-consent">
+	<div class="content notification-sticky__content">
+		<span class="notification-sticky__icon icon icon--info"></span>
+		<span class="notification-sticky__text"> {!! trans('cookieConsent::texts.message') !!}</span>
+		<button type="button" class="button notification-sticky__button js-cookie-consent-agree cookie-consent__agree">
+			<span class="button__label ">{{ trans('cookieConsent::texts.agree') }}</span>
+		</button>
+	</div>
 </div>
