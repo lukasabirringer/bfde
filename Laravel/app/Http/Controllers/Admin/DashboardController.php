8ae83a44fc3b4833d94ed59d6348ed8af8af5e46
Lukas Birringer <lukas.a.birringer@gmail.com>
2017-07-29 11:20:28 +0200

    Little

--- Laravel/app/Http/Controllers/Admin/DashboardController.php
+++ Laravel/app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Submittedbeachcourt;
 
 class DashboardController extends Controller
