813522b84183281dc2e86a30df96c0d8e4d4ae56
Lukas Birringer <lukas.a.birringer@gmail.com>
2017-08-13 19:39:32 +0200

    Added custom validation messages file

---
+++ Laravel/resources/lang/de/validation.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'custom' => [
+		    'name' => [
+		        'unique' => 'Leider ist dein Username schon vergeben',
+		    ],
+		    'email' => [
+		        'unique' => 'Leider ist deine E-Mail-Adresse schon vergeben',
+		    ],
+		],
+];
